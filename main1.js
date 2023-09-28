8ef63e059b9ae476f9aacf75b154bdedbd11e5a3
OneSpongeMan <146334098+OneSpongeMan@users.noreply.github.com>
2023-09-28 21:05:42 +0400

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,81 @@
+let a = Math.floor(Math.random() * 100);
+
+if (a > 10) {
+    if (a > 5) {
+        if ((2 * a + 1) > 4) {
+            console.log(5)
+        } else {
+            switch (a % 2) {
+                case 0:
+                    console.log(6)
+                    break
+                default:
+                    console.log(7)
+                    break
+            }
+        }
+    } else {
+        if (a < 3) {
+            switch (a % 2) {
+                case 0:
+                    console.log(6)
+                    break
+                default:
+                    console.log(7)
+                    break
+            }
+        } else {
+            if (2 * (a - 2) > 4) {
+                console.log(5)
+            } else {
+                switch (a % 2) {
+                    case 0:
+                        console.log(6)
+                        break
+                    default:
+                        console.log(7)
+                        break
+                }
+            }
+        }
+    }
+} else {
+    if (a * 2 > 5) {
+        if ((2 * a + 1) > 4) {
+            console.log(5)
+        } else {
+            switch (a % 2) {
+                case 0:
+                    console.log(6)
+                    break
+                default:
+                    console.log(7)
+                    break
+            }
+        }
+    } else {
+        if (a < 3) {
+            switch (a % 2) {
+                case 0:
+                    console.log(6)
+                    break
+                default:
+                    console.log(7)
+                    break
+            }
+        } else {
+            if (2 * (a - 2) > 4) {
+                console.log(5)
+            } else {
+                switch (a % 2) {
+                    case 0:
+                        console.log(6)
+                        break
+                    default:
+                        console.log(7)
+                        break
+                }
+            }
+        }
+    }
+}
